c93430e4ddd3195fd59f02f71a95893ec6209dbf
Yan Su <tsu@yaroot.net>
2019-12-20 17:09:40 +0900

    add transaction test

--- src/test/scala/redis4s/FreeSuite.scala
+++ src/test/scala/redis4s/FreeSuite.scala
@@ -8,11 +8,27 @@ import cats.effect.minitest.IOTestSuite
 object FreeSuite extends IOTestSuite {
   val session: RedisSession[IO]    = RedisTest.newSession()
   val client: RedisClient[RedisIO] = RedisIO.client
-  RedisTest.flushAll(session)
 
   test("run pipeline") {
+    RedisTest.flushAll(session)
     for {
       r <- session.run(client.set("foo", "bar") *> client.get("foo"))
     } yield assertEquals(r, "bar".some)
+  }
+
+  test("run transaction") {
+    RedisTest.flushAll(session)
+    for {
+      r <- session.transact(client.set("foo", "bar") *> client.set("foo", "baz") *> client.get("foo"))
+    } yield assertEquals(r, "baz".some)
+  }
+
+  test("run transaction with watch") {
+    RedisTest.flushAll(session)
+    val ops = client.set("foo", "bar") *> client.set("foo", "baz") *> client.get("foo")
+    val keys = Vector("foo", "bar", "baz")
+    for {
+      r <- session.transact(ops, keys)
+    } yield assertEquals(r, "baz".some)
   }
 }
